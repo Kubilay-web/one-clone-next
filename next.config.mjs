2ededb12d0805edb4453d127a237e3ec5cbfe378
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-03-27 21:46:33 +0300

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,15 @@ const nextConfig = {
   serverExternalPackages: ["@node-rs/argon2"],
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "https",
+      //   hostname: "utfs.io",
+      //   pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+      // },
       {
         protocol: "https",
-        hostname: "utfs.io",
-        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+        hostname: "sb52wuzhjx.ufs.sh",
+        pathname: `/f/*`,
       },
     ],
     unoptimized: true,
