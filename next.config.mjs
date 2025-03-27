c4b7e0f01cf185a5dfb54331426fb2e5950f3eca
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-03-27 18:45:51 +0300

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const uploadthingAppId = process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID;
 
+// Ortam değişkeni kontrolü
+const uploadthingAppId = process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID;
 if (!uploadthingAppId) {
   throw new Error(
     "NEXT_PUBLIC_UPLOADTHING_APP_ID environment variable is not defined",
@@ -19,11 +20,25 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "utfs.io",
-        pathname: `/a/${uploadthingAppId}/*`, // burası artık sabit string
+        pathname: `/a/${uploadthingAppId}/**`,
+      },
+      {
+        protocol: "https",
+        hostname: "**.ufs.sh",
+        pathname: "/a/**",
       },
     ],
+    // Tüm yaygın device ve image boyutları
+    deviceSizes: [
+      320, 375, 414, 480, 640, 768, 828, 1024, 1080, 1200, 1280, 1366, 1440,
+      1536, 1600, 1920, 2048, 2560, 3840,
+    ],
+    imageSizes: [
+      16, 24, 32, 48, 64, 96, 128, 256, 384, 512, 640, 768, 1024, 1280, 1600,
+      1920, 2048, 2560, 3840,
+    ],
   },
-  rewrites: () => {
+  rewrites: async () => {
     return [
       {
         source: "/hashtag/:tag",
