37c9e46cf874afef01c0bcc1d5488240a3e88279
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-03-27 19:50:29 +0300

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-// Ortam değişkeni kontrolü
-const uploadthingAppId = process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID;
-if (!uploadthingAppId) {
-  throw new Error(
-    "NEXT_PUBLIC_UPLOADTHING_APP_ID environment variable is not defined",
-  );
-}
-
 const nextConfig = {
   experimental: {
     staleTimes: {
@@ -16,21 +7,15 @@ const nextConfig = {
   },
   serverExternalPackages: ["@node-rs/argon2"],
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "utfs.io",
-        pathname: `/a/${uploadthingAppId}/**`, // Yerel ortam için
-      },
-      {
-        protocol: "https",
-        hostname: "**.ufs.sh", // Vercel'deki domain
-        pathname: "/a/**", // Tüm alt yolları kapsar
+        hostname: "sb52wuzhjx.ufs.sh",
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
       },
     ],
   },
-  rewrites: async () => {
+  rewrites: () => {
     return [
       {
         source: "/hashtag/:tag",
