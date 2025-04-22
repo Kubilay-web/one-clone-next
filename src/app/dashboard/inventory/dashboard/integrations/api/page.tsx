86f313ec571595f737fdcc297827ae1d29544ad4
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-04-22 13:59:26 +0300

    Updated

--- src/app/dashboard/inventory/dashboard/integrations/api/page.tsx
+++
@@ -1,5 +0,0 @@
-// import { getOrgApiKeys } from "@/actions/apiKeys";
-
-export default async function Home() {
-  return <main className="container mx-auto px-4 py-8"></main>;
-}
--- src/app/dashboard/inventory/dashboard/inventory/categories/new/page.tsx
+++
@@ -1,6 +0,0 @@
-// import CategoryForm from "../../../../../../../components/inventory/Forms/CategoryForm";
-import React from "react";
-
-export default function page() {
-  return <div className="p-8">{/* <CategoryForm /> */}</div>;
-}
