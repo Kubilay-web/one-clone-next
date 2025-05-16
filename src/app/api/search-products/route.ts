0a80f9545d59aa6b117e8f2db59c6e1b1c157b1a
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-05-16 13:56:31 +0300

    Updated

--- src/app/api/search-products/route.ts
+++ src/app/api/search-products/route.ts
@@ -1,8 +1,7 @@
-// pages/api/search-products.ts
 import client from "@/lib/elasticsearch";
 import { NextResponse } from "next/server";
 
-// Ürün tipi tanımlaması
+// Define product type
 interface Product {
   name: string;
 }
@@ -13,28 +12,34 @@ export async function GET(req: Request) {
 
   if (!q || typeof q !== "string") {
     return NextResponse.json(
-      { message: "Geçersiz arama sorgusu" },
+      {
+        message: "Invalid search query",
+      },
       { status: 400 },
     );
   }
 
   try {
-    // Elasticsearch sorgusu
+    // Query Elasticsearch with improved types
     const response = await client.search<{ _source: Product }>({
-      index: "products", // Elasticsearch index adını kontrol edin
+      index: "products",
       body: {
         query: {
           match: {
+            // match is more efficient for typical searches
             name: q,
           },
         },
       },
     });
 
+    // Extract the results
     const results = response.hits.hits.map((hit) => hit._source);
+
+    // Return results as a JSON response
     return NextResponse.json(results);
   } catch (error: any) {
-    console.error("Elasticsearch hatası:", error);
+    // Log the error and return a response
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 }
