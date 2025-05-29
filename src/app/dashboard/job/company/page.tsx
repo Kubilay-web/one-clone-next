f79b647a7113e6a9a5a9708a950d2ccd00ebb5da
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-05-29 12:48:13 +0300

    Updated

--- src/app/dashboard/job/company/page.tsx
+++ src/app/dashboard/job/company/page.tsx
@@ -1,12 +1,5 @@
 import React, { useEffect } from "react";
 
 export default function page() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    import("bootstrap/dist/css/bootstrap.min.css");
-    import(
-      "bootstrap-material-design/dist/css/bootstrap-material-design.min.css"
-    );
-  }, []);
   return <div>Company</div>;
 }
