03a685f2cf26188cf17c2c02594a5780a99935b2
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-07-10 20:28:58 +0300

    Updated

--- src/components/stackoverflow/forms/QuestionForm.tsx
+++ src/components/stackoverflow/forms/QuestionForm.tsx
@@ -155,7 +155,7 @@ const QuestionForm = ({ question, isEdit = false }: Params) => {
                 />
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
-                Be specific and imagine you're asking a question to another
+                Be specific and imagine you are asking a question to another
                 person.
               </FormDescription>
               <FormMessage />
@@ -179,7 +179,7 @@ const QuestionForm = ({ question, isEdit = false }: Params) => {
                 />
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
-                Introduce the problem and expand on what you've put in the
+                Introduce the problem and expand on what you ve put in the
                 title.
               </FormDescription>
               <FormMessage />
